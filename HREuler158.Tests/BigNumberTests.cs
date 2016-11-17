7293b39dc6daca37f69f0230133e59af5e7eb66b
Krisztian Daroczi <daroczi.krisztian@gmail.com>
2016-11-17 20:17:02 +0200

    Add a big number class with addition and basic operators.

---
+++ HREuler158.Tests/BigNumberTests.cs
@@ -0,0 +1,203 @@
+﻿using HackerRankEuler158;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HREuler158.Tests
+{
+	[TestClass]
+	public class BigNumberTests
+	{
+		[TestMethod]
+		public void LessAndGreater_OneDigit_LeftLTRight()
+		{
+			BigNumber left = new BigNumber(4);
+			BigNumber right = new BigNumber(5);
+			bool leftLessThanRight = left < right;
+			bool leftGreaterThanRight = left > right;
+
+			Assert.AreEqual(true, leftLessThanRight);
+			Assert.AreEqual(false, leftGreaterThanRight);
+		}
+
+		[TestMethod]
+		public void LessAndGreater_TwoDigit_LeftLTRight()
+		{
+			BigNumber left = new BigNumber(33);
+			BigNumber right = new BigNumber(99);
+			bool leftLessThanRight = left < right;
+			bool leftGreaterThanRight = left > right;
+
+			Assert.AreEqual(true, leftLessThanRight);
+			Assert.AreEqual(false, leftGreaterThanRight);
+		}
+
+		[TestMethod]
+		public void LessAndGreater_MixedDigitSecondLonger_LeftGTRight()
+		{
+			BigNumber left = new BigNumber(333);
+			BigNumber right = new BigNumber(4564567);
+			bool leftLessThanRight = left < right;
+			bool leftGreaterThanRight = left > right;
+
+			Assert.AreEqual(true, leftLessThanRight);
+			Assert.AreEqual(false, leftGreaterThanRight);
+		}
+
+		[TestMethod]
+		public void LessAndGreater_MixedDigitFirstLonger_LeftLTRight()
+		{
+			BigNumber left = new BigNumber("8797987899");
+			BigNumber right = new BigNumber(456456);
+			bool leftLessThanRight = left < right;
+			bool leftGreaterThanRight = left > right;
+
+			Assert.AreEqual(false, leftLessThanRight);
+			Assert.AreEqual(true, leftGreaterThanRight);
+		}
+
+		[TestMethod]
+		public void LessAndGreater_LongEqualDigits_LeftEQRight()
+		{
+			BigNumber left  = new BigNumber("11111111111111111111");
+			BigNumber right = new BigNumber("11111111111111111111");
+			bool leftLessThanRight = left < right;
+			bool leftGreaterThanRight = left > right;
+
+			Assert.AreEqual(false, leftLessThanRight);
+			Assert.AreEqual(false, leftGreaterThanRight);
+		}
+
+		[TestMethod]
+		public void LessAndGreater_TwoDigit_LeftEQRight()
+		{
+			BigNumber left = new BigNumber(73);
+			BigNumber right = new BigNumber(73);
+			bool leftLessThanRight = left < right;
+			bool leftGreaterThanRight = left > right;
+
+			Assert.AreEqual(false, leftLessThanRight);
+			Assert.AreEqual(false, leftGreaterThanRight);
+		}
+
+		[TestMethod]
+		public void Add_OneDigit_CorrectAddition()
+		{
+			BigNumber left = new BigNumber(4);
+			BigNumber right = new BigNumber(5);
+			BigNumber result = left + right;
+
+			Assert.AreEqual("9", result.Value);
+		}
+
+		[TestMethod]
+		public void Add_TwoDigit_CorrectAddition()
+		{
+			BigNumber left = new BigNumber(11);
+			BigNumber right = new BigNumber(11);
+			BigNumber result = left + right;
+
+			Assert.AreEqual("22", result.Value);
+		}
+
+		[TestMethod]
+		public void Add_OneDigitWithCarry_CorrectAddition()
+		{
+			BigNumber left = new BigNumber(9);
+			BigNumber right = new BigNumber(8);
+			BigNumber result = left + right;
+
+			Assert.AreEqual("17", result.Value);
+		}
+
+		[TestMethod]
+		public void Add_FirstNumberLongerWithCarry_CorrectAddition()
+		{
+			BigNumber left = new BigNumber(999);
+			BigNumber right = new BigNumber(88);
+			BigNumber result = left + right;
+
+			Assert.AreEqual("1087", result.Value);
+		}
+
+		[TestMethod]
+		public void Add_SecondNumberLongerWithCarry_CorrectAddition()
+		{
+			BigNumber left = new BigNumber(88);
+			BigNumber right = new BigNumber(999);
+			BigNumber result = left + right;
+
+			Assert.AreEqual("1087", result.Value);
+		}
+
+		[TestMethod]
+		public void Multiply_OneDigit_CorrectMultiplication()
+		{
+			BigNumber left = new BigNumber(2);
+			BigNumber right = new BigNumber(4);
+			BigNumber result = left * right;
+
+			Assert.AreEqual("8", result.Value);
+		}
+
+		[TestMethod]
+		public void Multiply_OneDigit_CorrectMultiplicationResultingDoubleDigit()
+		{
+			BigNumber left = new BigNumber(8);
+			BigNumber right = new BigNumber(9);
+			BigNumber result = left * right;
+
+			Assert.AreEqual("72", result.Value);
+		}
+
+		[TestMethod]
+		public void Multiply_TwoDigit_CorrectMultiplicationResultingThreeDigits()
+		{
+			BigNumber left = new BigNumber(18);
+			BigNumber right = new BigNumber(25);
+			BigNumber result = left * right;
+
+			Assert.AreEqual("450", result.Value);
+		}
+
+		[TestMethod]
+		public void Multiply_TwoDigit_CorrectMultiplicationResultingFourDigits()
+		{
+			BigNumber left = new BigNumber(88);
+			BigNumber right = new BigNumber(99);
+			BigNumber result = left * right;
+
+			Assert.AreEqual("8712", result.Value);
+		}
+
+		[TestMethod]
+		public void Multiply_MixedDigitsFirstLonger_CorrectMultiplication()
+		{
+			BigNumber left = new BigNumber(987654);
+			BigNumber right = new BigNumber(123);
+			BigNumber result = left * right;
+
+			Assert.AreEqual("121481442", result.Value);
+		}
+
+		[TestMethod]
+		public void Multiply_MixedDigitsSecondLonger_CorrectMultiplication()
+		{
+			BigNumber left = new BigNumber(123);
+			BigNumber right = new BigNumber(987654);
+			BigNumber result = left * right;
+
+			Assert.AreEqual("121481442", result.Value);
+		}
+
+		[TestMethod]
+		[Ignore]
+		public void Multiply_SixDigitNumbers_CorrectMultiplication()
+		{
+			BigNumber left = new BigNumber(987654);
+			BigNumber right = new BigNumber(987654);
+			// multiplication is not fast enough
+			BigNumber result = left * right;
+
+			Assert.AreEqual("975460423716", result.Value);
+		}
+	}
+}
---
+++ HREuler158/BigNumber.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace HackerRankEuler158
+{
+	public class BigNumber
+	{
+		private IEnumerable<byte> _innerValue;
+
+		public string Value
+		{
+			get { return string.Join(string.Empty, _innerValue).TrimStart('0'); }
+		}
+
+		public BigNumber(int number)
+		{
+			InitValue(number.ToString());
+		}
+
+		public BigNumber(string number)
+		{
+			InitValue(number);
+		}
+
+		public BigNumber(BigNumber other)
+		{
+			_innerValue = other._innerValue;
+		}
+
+		public BigNumber(byte[] values)
+		{
+			_innerValue = values;
+		}
+
+		private void InitValue(string number)
+		{
+			_innerValue = number.Select(x => byte.Parse(x.ToString()));
+		}
+		private void InitValue(byte[] numbers)
+		{
+			_innerValue = numbers.AsEnumerable();
+		}
+
+		public static BigNumber operator + (BigNumber left, BigNumber right)
+		{
+			return left.Add(right);
+		}
+
+		public static BigNumber operator ++ (BigNumber val)
+		{
+			BigNumber one = new BigNumber(1);
+			return val.Add(one);
+		}
+
+		public static bool operator < (BigNumber left, BigNumber right)
+		{
+			if (left == right)
+				return false;
+			return LeftLessThanRight(left, right);
+		}
+
+		public static bool operator > (BigNumber left, BigNumber right)
+		{
+			if (left == right)
+				return false;
+			return !LeftLessThanRight(left, right);
+		}
+
+		public static bool operator == (BigNumber left, BigNumber right)
+		{
+			if (ReferenceEquals(left, right))
+				return true;
+
+			if (((object)left == null) || ((object)right == null))
+				return false;
+
+			return left.Equals(right);
+		}
+
+		public static bool operator != (BigNumber left, BigNumber right)
+		{
+			return !(left == right);
+		}
+
+		public static BigNumber operator * (BigNumber left, BigNumber right)
+		{
+			BigNumber smaller = left;
+			BigNumber bigger = right;
+			if (smaller > bigger)
+				Swap(ref smaller, ref bigger);
+
+			BigNumber productSum = new BigNumber(0);
+			for (BigNumber i = new BigNumber(0); i < smaller; i++)
+				productSum += bigger;
+
+			return productSum;
+		}
+
+		public override bool Equals(object obj)
+		{
+			if (obj == null)
+				return false;
+
+			BigNumber b = obj as BigNumber;
+			if ((System.Object)b == null)
+				return false;
+
+			return this.Equals(b);
+		}
+
+		public bool Equals(BigNumber b)
+		{
+			if ((object)b == null)
+				return false;
+
+			return string.Compare(Value, b.Value) == 0;
+		}
+
+		public override int GetHashCode()
+		{
+			return Value.GetHashCode();
+		}
+
+		private BigNumber Add(BigNumber number)
+		{
+			byte[] longer = _innerValue.ToArray();
+			byte[] shorter = number._innerValue.ToArray();
+			if (longer.Length < shorter.Length)
+				Swap(ref longer, ref shorter);
+
+			int n = longer.Length + 1;
+			byte[] shorterPadded = new byte[longer.Length];
+			int startAt = shorterPadded.Length - shorter.Length;
+			Buffer.BlockCopy(shorter, 0, shorterPadded, startAt, shorter.Length);
+
+			byte[] result = new byte[n];
+
+			byte lastCarry = 0;
+			for (int i = longer.Length - 1; i >= 0; i--)
+			{
+				byte digitSum = (byte)(longer[i] + shorterPadded[i] + lastCarry);
+				byte resultDigit = 0;
+				byte carry = 0;
+
+				if (digitSum > 9)
+				{
+					resultDigit = (byte)(digitSum % 10);
+					carry = 1;
+				}
+				else
+				{
+					resultDigit = digitSum;
+				}
+
+				result[--n] = resultDigit;
+
+				lastCarry = carry;
+			}
+			if (lastCarry > 0)
+				result[0] = lastCarry;
+			return new BigNumber(result);
+		}
+
+		private static bool LeftLessThanRight(BigNumber left, BigNumber right)
+		{
+			string leftVal = left.Value;
+			string rightVal = right.Value;
+			if (leftVal.Length < rightVal.Length)
+				return true;
+
+			if (leftVal.Length > rightVal.Length)
+				return false;
+
+			for (int i = 0; i < leftVal.Length; i++)
+			{
+				byte leftByte = byte.Parse(leftVal[i].ToString());
+				byte rightByte = byte.Parse(rightVal[i].ToString());
+
+				if (leftByte < rightByte)
+					return true;
+			}
+
+			return false;
+		}
+
+		private static void Swap<T>(ref T lhs, ref T rhs)
+		{
+			T temp;
+			temp = lhs;
+			lhs = rhs;
+			rhs = temp;
+		}
+	}
+}
