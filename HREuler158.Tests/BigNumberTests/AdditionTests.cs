77b4e798fc70d5ff8e3405b828fcca053407b240
Krisztian Daroczi <daroczi.krisztian@gmail.com>
2016-11-20 16:43:43 +0200

    Introduce addition tests with negative numbers.

--- HREuler158.Tests/BigNumberTests/AdditionTests.cs
+++ HREuler158.Tests/BigNumberTests/AdditionTests.cs
@@ -7,6 +7,16 @@ namespace HREuler158.Tests.BigNumberTests
 	public class AdditionTests
 	{
 		[TestMethod]
+		public void Add_ZeroPlusZero_Zero()
+		{
+			BigNumber left = new BigNumber(0);
+			BigNumber right = new BigNumber(0);
+			BigNumber result = left + right;
+
+			Assert.AreEqual("0", result.Value);
+		}
+
+		[TestMethod]
 		public void Add_OneDigit_CorrectAddition()
 		{
 			BigNumber left = new BigNumber(4);
@@ -54,6 +64,76 @@ namespace HREuler158.Tests.BigNumberTests
 			BigNumber result = left + right;
 
 			Assert.AreEqual("1087", result.Value);
+		}
+
+		[TestMethod]
+		public void Add_FirstNumberNegative_ResultPositive()
+		{
+			BigNumber left = new BigNumber(-8);
+			BigNumber right = new BigNumber(9);
+			BigNumber result = left + right;
+
+			Assert.AreEqual("1", result.Value);
+		}
+
+		[TestMethod]
+		public void Add_SecondNumberNegative_ResultPositive()
+		{
+			BigNumber left = new BigNumber(8);
+			BigNumber right = new BigNumber(-7);
+			BigNumber result = left + right;
+
+			Assert.AreEqual("1", result.Value);
+		}
+
+		[TestMethod]
+		public void Add_FirstNumberNegative_ResultNegative()
+		{
+			BigNumber left = new BigNumber(-8);
+			BigNumber right = new BigNumber(12);
+			BigNumber result = left + right;
+
+			Assert.AreEqual("-4", result.Value);
+		}
+
+		[TestMethod]
+		public void Add_SecondNumberNegative_ResultNegative()
+		{
+			BigNumber left = new BigNumber(8);
+			BigNumber right = new BigNumber(-10);
+			BigNumber result = left + right;
+
+			Assert.AreEqual("-2", result.Value);
+		}
+
+		[TestMethod]
+		public void Add_BothNegative_ResultNegative()
+		{
+			BigNumber left = new BigNumber(-98);
+			BigNumber right = new BigNumber(-89);
+			BigNumber result = left + right;
+
+			Assert.AreEqual("-187", result.Value);
+		}
+
+		[TestMethod]
+		public void Add_FirstNumberNegative_ResultZero()
+		{
+			BigNumber left = new BigNumber(-8);
+			BigNumber right = new BigNumber(8);
+			BigNumber result = left + right;
+
+			Assert.AreEqual("0", result.Value);
+		}
+
+		[TestMethod]
+		public void Add_SecondNumberNegative_ResultZero()
+		{
+			BigNumber left = new BigNumber(10);
+			BigNumber right = new BigNumber(-10);
+			BigNumber result = left + right;
+
+			Assert.AreEqual("0", result.Value);
 		}
 	}
 }
