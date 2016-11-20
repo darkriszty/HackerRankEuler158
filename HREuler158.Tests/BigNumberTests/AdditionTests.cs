09db8b9d92da29faa9fd270156a00a2ffff1e461
Krisztian Daroczi <daroczi.krisztian@gmail.com>
2016-11-20 16:31:24 +0200

    Break up the big number tests into multiple classes.

---
+++ HREuler158.Tests/BigNumberTests/AdditionTests.cs
@@ -0,0 +1,59 @@
+﻿using HackerRankEuler158;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HREuler158.Tests.BigNumberTests
+{
+	[TestClass]
+	public class AdditionTests
+	{
+		[TestMethod]
+		public void Add_OneDigit_CorrectAddition()
+		{
+			BigNumber left = new BigNumber(4);
+			BigNumber right = new BigNumber(5);
+			BigNumber result = left + right;
+
+			Assert.AreEqual("9", result.Value);
+		}
+
+		[TestMethod]
+		public void Add_TwoDigit_CorrectAddition()
+		{
+			BigNumber left = new BigNumber(11);
+			BigNumber right = new BigNumber(11);
+			BigNumber result = left + right;
+
+			Assert.AreEqual("22", result.Value);
+		}
+
+		[TestMethod]
+		public void Add_OneDigitWithCarry_CorrectAddition()
+		{
+			BigNumber left = new BigNumber(9);
+			BigNumber right = new BigNumber(8);
+			BigNumber result = left + right;
+
+			Assert.AreEqual("17", result.Value);
+		}
+
+		[TestMethod]
+		public void Add_FirstNumberLongerWithCarry_CorrectAddition()
+		{
+			BigNumber left = new BigNumber(999);
+			BigNumber right = new BigNumber(88);
+			BigNumber result = left + right;
+
+			Assert.AreEqual("1087", result.Value);
+		}
+
+		[TestMethod]
+		public void Add_SecondNumberLongerWithCarry_CorrectAddition()
+		{
+			BigNumber left = new BigNumber(88);
+			BigNumber right = new BigNumber(999);
+			BigNumber result = left + right;
+
+			Assert.AreEqual("1087", result.Value);
+		}
+	}
+}
---
+++ HREuler158.Tests/BigNumberTests/ComparisonTests.cs
@@ -0,0 +1,81 @@
+﻿using HackerRankEuler158;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HREuler158.Tests.BigNumberTests
+{
+	[TestClass]
+	public class ComparisonTests
+	{
+		[TestMethod]
+		public void LessAndGreater_OneDigit_LeftLTRight()
+		{
+			BigNumber left = new BigNumber(4);
+			BigNumber right = new BigNumber(5);
+			bool leftLessThanRight = left < right;
+			bool leftGreaterThanRight = left > right;
+
+			Assert.AreEqual(true, leftLessThanRight);
+			Assert.AreEqual(false, leftGreaterThanRight);
+		}
+
+		[TestMethod]
+		public void LessAndGreater_TwoDigit_LeftLTRight()
+		{
+			BigNumber left = new BigNumber(33);
+			BigNumber right = new BigNumber(99);
+			bool leftLessThanRight = left < right;
+			bool leftGreaterThanRight = left > right;
+
+			Assert.AreEqual(true, leftLessThanRight);
+			Assert.AreEqual(false, leftGreaterThanRight);
+		}
+
+		[TestMethod]
+		public void LessAndGreater_MixedDigitSecondLonger_LeftGTRight()
+		{
+			BigNumber left = new BigNumber(333);
+			BigNumber right = new BigNumber(4564567);
+			bool leftLessThanRight = left < right;
+			bool leftGreaterThanRight = left > right;
+
+			Assert.AreEqual(true, leftLessThanRight);
+			Assert.AreEqual(false, leftGreaterThanRight);
+		}
+
+		[TestMethod]
+		public void LessAndGreater_MixedDigitFirstLonger_LeftLTRight()
+		{
+			BigNumber left = new BigNumber("8797987899");
+			BigNumber right = new BigNumber(456456);
+			bool leftLessThanRight = left < right;
+			bool leftGreaterThanRight = left > right;
+
+			Assert.AreEqual(false, leftLessThanRight);
+			Assert.AreEqual(true, leftGreaterThanRight);
+		}
+
+		[TestMethod]
+		public void LessAndGreater_LongEqualDigits_LeftEQRight()
+		{
+			BigNumber left = new BigNumber("11111111111111111111");
+			BigNumber right = new BigNumber("11111111111111111111");
+			bool leftLessThanRight = left < right;
+			bool leftGreaterThanRight = left > right;
+
+			Assert.AreEqual(false, leftLessThanRight);
+			Assert.AreEqual(false, leftGreaterThanRight);
+		}
+
+		[TestMethod]
+		public void LessAndGreater_TwoDigit_LeftEQRight()
+		{
+			BigNumber left = new BigNumber(73);
+			BigNumber right = new BigNumber(73);
+			bool leftLessThanRight = left < right;
+			bool leftGreaterThanRight = left > right;
+
+			Assert.AreEqual(false, leftLessThanRight);
+			Assert.AreEqual(false, leftGreaterThanRight);
+		}
+	}
+}
