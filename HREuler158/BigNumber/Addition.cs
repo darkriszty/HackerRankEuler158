cb8b2477baa3c14bf2236e11cd22d68a26865ba1
Krisztian Daroczi <daroczi.krisztian@gmail.com>
2016-11-24 21:43:54 +0200

    Move the zero padding into the utils.

--- HREuler158/BigNumber/Addition.cs
+++ HREuler158/BigNumber/Addition.cs
@@ -1,28 +1,22 @@
-﻿using System;
-using System.Linq;
-
-namespace HackerRankEuler158
+﻿namespace HackerRankEuler158
 {
 	public partial class BigNumber
 	{
 		private BigNumber Add(BigNumber number)
 		{
-			byte[] longer = _innerValue.ToArray();
-			byte[] shorter = number._innerValue.ToArray();
-			if (longer.Length < shorter.Length)
-				Utils.Swap(ref longer, ref shorter);
+			var normalized = Utils.PrepareForAddition(this, number);
+			byte[] left = normalized.Item1;
+			byte[] right = normalized.Item2;
+			int maxLength = normalized.Item3;
 
-			int n = longer.Length + 1;
-			byte[] shorterPadded = new byte[longer.Length];
-			int startAt = shorterPadded.Length - shorter.Length;
-			Buffer.BlockCopy(shorter, 0, shorterPadded, startAt, shorter.Length);
 
+			int n = maxLength + 1;
 			byte[] result = new byte[n];
 
 			byte lastCarry = 0;
-			for (int i = longer.Length - 1; i >= 0; i--)
+			for (int i = left.Length - 1; i >= 0; i--)
 			{
-				byte digitSum = (byte)(longer[i] + shorterPadded[i] + lastCarry);
+				byte digitSum = (byte)(left[i] + right[i] + lastCarry);
 				byte resultDigit = 0;
 				byte carry = 0;
 
--- HREuler158/Utils.cs
+++ HREuler158/Utils.cs
@@ -1,4 +1,7 @@
-﻿namespace HackerRankEuler158
+﻿using System;
+using System.Linq;
+
+namespace HackerRankEuler158
 {
 	public class Utils
 	{
@@ -8,6 +11,32 @@
 			temp = lhs;
 			lhs = rhs;
 			rhs = temp;
+		}
+
+		public static Tuple<byte[], byte[], int> PrepareForAddition(BigNumber lhs, BigNumber rhs)
+		{
+			byte[] left = lhs.ValueAsBytes.ToArray();
+			byte[] right = rhs.ValueAsBytes.ToArray();
+
+			int maxLength = System.Math.Max(left.Length, right.Length);
+
+			left = PadZero(left, maxLength);
+			right = PadZero(right, maxLength);
+
+			return Tuple.Create(left, right, maxLength);
+		}
+
+		public static byte[] PadZero(byte[] arrayToPad, int maxLength)
+		{
+			if (arrayToPad.Length < maxLength)
+			{
+				byte[] newLeft = new byte[maxLength];
+				int startAt = newLeft.Length - arrayToPad.Length;
+				Buffer.BlockCopy(arrayToPad, 0, newLeft, startAt, arrayToPad.Length);
+				return newLeft;
+			}
+
+			return arrayToPad;
 		}
 	}
 }
