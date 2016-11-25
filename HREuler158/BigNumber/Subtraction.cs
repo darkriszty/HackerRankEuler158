2b96e0599d236b6a7425d4ce8b1a36dbe21f44ce
Krisztian Daroczi <daroczi.krisztian@gmail.com>
2016-11-25 21:32:36 +0200

    Make the subtraction use the addition.

--- HREuler158/BigNumber/Subtraction.cs
+++ HREuler158/BigNumber/Subtraction.cs
@@ -1,10 +1,15 @@
-﻿namespace HackerRankEuler158
+﻿using System.Linq;
+
+namespace HackerRankEuler158
 {
 	public partial class BigNumber
 	{
 		private BigNumber Subtract(BigNumber number)
 		{
-			return new BigNumber(0);
+			bool newSignNegative = !number._isNegative;
+
+			BigNumber numberNegated = new BigNumber(number.ValueAsBytes.ToArray(), newSignNegative);
+			return Add(numberNegated);
 		}
 	}
 }
